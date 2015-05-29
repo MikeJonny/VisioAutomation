adaefbb63236153eb0ec97f92a08feb859a598eb
Saveen Reddy <saveenr@live.com>
2015-05-29 02:00:44 -0700

    Removed uneeded rectangle methods

--- VisioAutomation_2010/GeneralTreeLayout/Rectangle.cs
+++ VisioAutomation_2010/GeneralTreeLayout/Rectangle.cs
@@ -26,24 +26,6 @@
             this.Top = top;
         }
 
-        public Rectangle(Point lowerleft, Point upperright)
-            : this()
-        {
-            if (upperright.X < lowerleft.X)
-            {
-                throw new System.ArgumentException("left must be <=right");
-            }
-
-            if (upperright.Y < lowerleft.Y)
-            {
-                throw new System.ArgumentException("bottom must be <= top");
-            }
-
-            this.Left = lowerleft.X;
-            this.Bottom = lowerleft.Y;
-            this.Right = upperright.X;
-            this.Top = upperright.Y;
-        }
 
         public Rectangle(Point lowerleft, Size s)
             : this()
@@ -64,34 +46,7 @@
             this.Top = lowerleft.Y + s.Height;
         }
 
-        public static Rectangle FromCenterPoint(double x, double y, double w, double h)
-        {
-            if (w < 0)
-            {
-                throw new System.ArgumentOutOfRangeException(nameof(w), "width must be non-negative");
-            }
 
-            if (h < 0)
-            {
-                throw new System.ArgumentOutOfRangeException(nameof(h), "height must be non-negative");
-            }
-
-            var xradius = w/2.0;
-            var yradius = h/2.0;
-            var r = new Rectangle(x - xradius, y - yradius, x + xradius, y + yradius);
-            return r;
-        }
-
-        public static Rectangle FromCenterPoint(Point p, double width, double height)
-        {
-            return Rectangle.FromCenterPoint(p.X, p.Y, width, height);
-        }
-
-        public override string ToString()
-        {
-            string s = string.Format(System.Globalization.CultureInfo.InvariantCulture, "({0:0.#####},{1:0.#####},{2:0.#####},{3:0.#####})", this.Left, this.Bottom, this.Right, this.Top);
-            return s;
-        }
 
         public Point LowerLeft => new Point(this.Left, this.Bottom);
 
@@ -109,20 +64,6 @@
 
         public Point Center => new Point((this.Left + this.Right)/2.0, (this.Bottom + this.Top)/2.0);
 
-        public static Rectangle operator +(Rectangle r, Point p)
-        {
-            return r.Add(p.X, p.Y);
-        }
-
-        public static Rectangle operator -(Rectangle r, Point p)
-        {
-            return r.Subtract(p.X, p.Y);
-        }
-
-        public static Rectangle operator *(Rectangle r, double s)
-        {
-            return r.Multiply(s, s);
-        }
 
         public Rectangle Add(double dx, double dy)
         {
@@ -130,17 +71,6 @@
             return r2;
         }
 
-        public Rectangle Add(Size s)
-        {
-            var r2 = new Rectangle(this.Left + s.Width, this.Bottom + s.Height, this.Right + s.Width, this.Top + s.Height);
-            return r2;
-        }
-
-        public Rectangle Add(Point s)
-        {
-            var r2 = new Rectangle(this.Left + s.X, this.Bottom + s.Y, this.Right + s.X, this.Top + s.Y);
-            return r2;
-        }
 
 
         public Rectangle Subtract(double dx, double dy)
@@ -149,17 +79,6 @@
             return r2;
         }
 
-        public Rectangle Subtract(Size s)
-        {
-            var r2 = new Rectangle(this.Left - s.Width, this.Bottom - s.Height, this.Right - s.Width, this.Top - s.Height);
-            return r2;
-        }
-
-        public Rectangle Subtract(Point s)
-        {
-            var r2 = new Rectangle(this.Left - s.X, this.Bottom - s.Y, this.Right - s.X, this.Top - s.Y);
-            return r2;
-        }
 
 
         public Rectangle Multiply(double sx, double sy)
